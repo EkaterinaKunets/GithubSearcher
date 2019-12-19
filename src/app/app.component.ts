2962e3190d7ac3e1a3724bc88b0845992613084d
Ekaterina Kucenko <ekaterina.kucenko@binomo.com>
2019-12-19 23:19:25 +0300

    add reactive search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 export interface GhRepo {
   name: string
@@ -14,18 +15,21 @@ export interface GhRepo {
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent {
   ghRepos: GhRepo[] = []
 
   constructor(private http: HttpClient) {
 
   }
 
-  ngOnInit() {
-    this.http.get<GhRepo[]>('https://api.github.com/search/repositories?q=test&sort=stars')
+  findRepo(inputValue: string) {
+    if (inputValue.length < 3) {
+      return null
+    }
+    this.http.get<GhRepo[]>(`https://api.github.com/search/repositories?q=${inputValue}&sort=stars`)
       .subscribe(ghRepos => {
         this.ghRepos = ghRepos.items.slice(0, 10);
-        console.log(ghRepos.items)
+        // console.log(ghRepos.items)
       })
   }
 }
