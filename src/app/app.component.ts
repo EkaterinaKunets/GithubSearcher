836760862ad928611a663f50046d5545637db587
Ekaterina Kucenko <ekaterina.kucenko@binomo.com>
2019-12-19 22:20:32 +0300

    interface & API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export interface GhRepo {
+  name: string
+  stargazers_count: number
+  link: string
+  items: Array<any>
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'github-search';
+
+export class AppComponent implements OnInit {
+  ghRepos: GhRepo[] = []
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.http.get<GhRepo[]>('https://api.github.com/search/repositories?q=test&sort=stars')
+      .subscribe(ghRepos => {
+        this.ghRepos = ghRepos.items.slice(0, 10);
+        console.log(ghRepos.items)
+      })
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { RepoListComponent } from './repo-list/repo-list.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepoListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
