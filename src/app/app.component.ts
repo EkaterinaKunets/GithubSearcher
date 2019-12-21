a0e224342e82d9e80da871b3a29eb09f53b93650
Ekaterina Kucenko <ekaterina.kucenko@binomo.com>
2019-12-21 14:43:19 +0300

    try to make normal service (dont work)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { GhRepo } from './interfaces/gh-repo.interface'
-import { Observable } from 'rxjs';
+// import { GhRepo } from './interfaces/gh-repo.interface';
+import { FilterReposService} from './filterRepos.service';
+// import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Component({
@@ -11,25 +11,19 @@ import { map } from 'rxjs/operators';
 })
 
 export class AppComponent {
-  ghRepos: Observable<any>
-  filteredRepos: Observable<any>
   loading = false
 
-  constructor(private http: HttpClient) {
-    this.filteredRepos = this.ghRepos = this.http.get<any>(`https://api.github.com/search/repositories?q=test&sort=stars`)
-      .pipe(map(ob => ob.items.slice(0, 10)))
-  }
+  constructor(private filterReposService: FilterReposService) {}
 
   findRepo(inputValue: string) {
     if (inputValue.length < 3) {
       return null
     }
     this.loading = true
-    // this.filteredRepos = this.http.get<GhRepo[]>(`https://api.github.com/search/repositories?q=${inputValue}&sort=stars`)
-    //   .subscribe(ghRepos => {
-    //     this.ghRepos = ghRepos.items.slice(0, 10);
-    //     this.loading = false
-    //   })
+    console.log(this.findRepo.toString())
+    this.filterReposService.findRepo(inputValue)
+      .pipe(map(ob => ob.items.slice(0, 10)))
+    this.loading = false
   }
 }
 
---
+++ src/app/filterRepos.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { GhRepo} from './interfaces/gh-repo.interface';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class FilterReposService {
+  constructor(private http: HttpClient) {}
+  findRepo(inputValue: string): Observable<any> {
+    return this.http.get<GhRepo>(`https://api.github.com/search/repositories?q=${inputValue}&sort=stars`)
+  }
+}
