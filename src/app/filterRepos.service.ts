8ea6f6975edc99de7a82c0b5aef7773af6472acb
Марк Гладков <markgladkov@MacBook-Pro-Mark.local>
2019-12-21 17:09:28 +0300

    Rewrite filter

--- src/app/filterRepos.service.ts
+++ src/app/filterRepos.service.ts
@@ -2,11 +2,14 @@ import { Injectable } from '@angular/core';
 import { GhRepo } from './interfaces/gh-repo.interface';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class FilterReposService {
   constructor(private http: HttpClient) {}
   findRepo(inputValue: string): Observable<GhRepo> {
-    return this.http.get<GhRepo>(`https://api.github.com/search/repositories?q=${inputValue}&sort=stars`)
+    return this.http.get<any>(`https://api.github.com/search/repositories?q=${inputValue}&sort=stars`).pipe(
+      map(ob => ob.items.slice(0, 10)),
+    )
   }
 }
